2b347736ed21660bed86f7ade30391535d7e49d1
Mirajje <vova.tarasov.7777@gmail.com>
2022-04-03 13:20:20 +0300

    1

--- Assets/Bandits - Pixel Art/Demo/Bandit.cs
+++ Assets/Bandits - Pixel Art/Demo/Bandit.cs
@@ -25,7 +25,7 @@ public class Bandit : MonoBehaviour {
     {
         if (col.gameObject.name == "Tile" && !m_grounded)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y - 10f, transform.position.z);
         }
     }
 
